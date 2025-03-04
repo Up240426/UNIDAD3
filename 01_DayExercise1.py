c5fa76ff7f4f9b8e18e9d4987102ebe19e2bb171
Up240426 <up240426@alumnos.upa.edu.mx>
2025-03-04 09:49:49 -0600

    Add files via upload

---
+++ 01_DayExercise1.py
@@ -0,0 +1,24 @@
+#introduccion
+
+print(3 + 4) 
+print(3 - 4)
+print(3 / 4)
+print(3 * 4)
+print(3 ** 4)
+print(3 % 4)
+print(3 // 4)
+
+#Checking data types 
+
+print("Jorge")
+print("Baez")
+print("Mexico")
+print("I am enjoying 30 days of python")
+
+print(type(10) )
+print(type(9.8))
+print(type(4-4j) )
+print(type("Asabeneh","Python","Finland"))
+print(type ("Jorge")) 
+print(type("Baez"))
+print(type("Mexico ")) 

8883c9718cda9daf3a54971b44f404288a5f2f42
Up240426 <up240426@alumnos.upa.edu.mx>
2025-04-02 15:12:03 -0600

    Delete Paython/01_Dayexcercise2.py

--- Paython/01_Dayexcercise2.py
+++
@@ -1,6 +0,0 @@
-your_name = 'Jorge Alexis Baez Almanza'
-Family_name = 'Tovar Cortes'
-country = 'Mexico'
-print('your name:', your_name)
-print('Family name: ', Family_name)
-print('Country: ', country)
